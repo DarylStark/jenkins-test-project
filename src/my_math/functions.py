3708e24cc891a88d69c398cef57f7a2744adc812
Daryl Stark <daryl@dstark.nl>
2023-02-06 11:33:16 +0100

    Formatted code and made sure `factorial(0)` will always be `1`

--- src/my_math/functions.py
+++ src/my_math/functions.py
@@ -1,10 +1,9 @@
 """ Math fuctions """
 
+
 def factorial(number: int) -> int:
     if number < 0:
         raise ValueError('Number cannot be negative')
-    if number == 0:
-        return 0
-    if number == 1:
+    if number == 0 or number == 1:
         return 1
     return factorial(number - 1) * number
