172314620147885866bf1a898d9e00bd1e866994
Daryl Stark <daryl@dstark.nl>
2023-02-06 11:21:44 +0100

    Made sure negative numbers are not allowed for factorial

--- src/my_math/functions.py
+++ src/my_math/functions.py
@@ -1,6 +1,8 @@
 """ Math fuctions """
 
 def factorial(number: int) -> int:
+    if number < 0:
+        raise ValueError('Number cannot be negative')
     if number == 0:
         return 0
     if number == 1:
